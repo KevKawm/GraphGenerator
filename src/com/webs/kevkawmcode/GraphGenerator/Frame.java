603270676b9d64847482a6797ad02a81e5c68df5
Will Garrett <willgarrett610@gmail.com>
2014-12-13 01:22:13 -0700

    Started on GUI

--- src/com/webs/kevkawmcode/GraphGenerator/Frame.java
+++ src/com/webs/kevkawmcode/GraphGenerator/Frame.java
@@ -1,28 +1,97 @@
 package com.webs.kevkawmcode.GraphGenerator;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 
-public class Frame extends JFrame{
-	private static final long serialVersionUID = 1L;
+import com.webs.kevkawmcode.Parser.Equation;
+import com.webs.kevkawmcode.Parser.EquationParser;
 
+public class Frame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField equationField;
+	private final JPanel equationPanel;
+	
+	public final List<String> equationStrings = new ArrayList<String>();
+	public final List<Equation> equations = new ArrayList<Equation>();
+	
 	public static void main(String[] args) {
-		new Frame();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame frame = new Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+	
+	public Frame() {
+		setTitle("Graph Generator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		setBounds(100, 100, 900, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-	public Frame(){
-		new JFrame();
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(157, 11, 717, 606);
+		contentPane.add(panel);
+
+		equationPanel = new JPanel();
+		equationPanel.setBackground(Color.LIGHT_GRAY);
+		equationPanel.setBounds(10, 11, 137, 606);
+		contentPane.add(equationPanel);
+		equationPanel.setLayout(null);
 		
-		Display display = new Display(this);
-		this.add(display);
+		JButton btnAdd = new JButton("Add Equation");
+		btnAdd.setBounds(10, 628, 108, 23);
+		btnAdd.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				final String name = equationField.getText();
+				equationStrings.add(name);
+				equations.add(new Equation(EquationParser.parse(name)));
+				addEquation(name);
+			}
+		});
+		contentPane.add(btnAdd);
 
-		this.setUndecorated(true);
-		this.setExtendedState(MAXIMIZED_BOTH);
-		this.setSize(800,630);
-		this.setResizable(false);
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
-		this.setTitle("Graph Generator");
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		equationField = new JTextField();
+		equationField.setToolTipText("Enter equation here");
+		equationField.setBounds(128, 629, 240, 20);
+		contentPane.add(equationField);
+		equationField.setColumns(10);
+	}
+	
+	public void addEquation(final String name){
+		final JCheckBox checkBox = new JCheckBox(name);
+		checkBox.setBackground(Color.LIGHT_GRAY);
+		checkBox.setBounds(6, equationPanel.getComponentCount() * 26 + 7, 125, 23);
+		equationPanel.add(checkBox);
+		paintComponents(getGraphics());
 	}
 	
 }
--- src/com/webs/kevkawmcode/GraphGenerator/Graph.java
+++ src/com/webs/kevkawmcode/GraphGenerator/Graph.java
@@ -1,13 +1,30 @@
 package com.webs.kevkawmcode.GraphGenerator;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Graph {
+import javax.swing.JPanel;
+
+public class Graph extends JPanel{
 
 	Dimension interval;
+	double zoom;
 	
-	public Graph(Dimension interval){
+	public Graph(Dimension interval, double zoom){
 		this.interval = interval;
+		this.zoom = zoom;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		
+	}
+	
+	private Point convert(Point p){
+		Point ret = new Point(p.x,p.y);
+		ret.y = getHeight() - ret.y;
+		return p;
 	}
 	
 }
