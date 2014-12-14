2ec3f583f0eae993aaab6beab2f0ceb23bec3acf
SnapHacker <malettabruno@gmail.com>
2014-12-14 20:25:53 -0300

    Improved parser

--- src/com/webs/kevkawmcode/Parser/EquationParser.java
+++ src/com/webs/kevkawmcode/Parser/EquationParser.java
@@ -6,15 +6,54 @@ import java.util.List;
 public class EquationParser {
 
 	public static List<String> parse(String eq) {
+		// Removing spaces
+		String eqBU = eq;
+		eq = "";
+		for (int i = 0; i < eqBU.length(); i++) {
+			if (eqBU.charAt(i) != ' ')
+				eq += eqBU.charAt(i);
+		}
+		// Remove some operations
+			// At beginning
+		while (true) {
+			if (eq.startsWith("+") || eq.startsWith("*") || eq.startsWith("/") || eq.startsWith("!")) {
+				eq = eq.substring(1);
+			} else {
+				break;
+			}
+		}
+			// At end
+		while (true) {
+			if (eq.endsWith("+") || eq.endsWith("-") || eq.endsWith("*") || eq.endsWith("/")) {
+				eq = eq.substring(0, eq.length() - 1);
+			} else {
+				break;
+			}
+		}
+		// Separating everything
 		List<String> ret_ = new ArrayList<String>();
 		String str = "";
 		for (int i = 0; i < eq.length(); i++) {
 			Character xar = eq.charAt(i);
 			if (ops().contains(xar.toString())) {
-				if (!str.equals(""))
-					ret_.add(str);
-				str = "";
-				ret_.add(xar.toString());
+				if (xar.toString().equals("-")) {
+					if (!isNumber(Character.toString(eq.charAt(i + 1)))) {
+						ret_.add(str);
+						str = "-";
+					} else if (i != 0) {
+						if (!str.equals(""))
+							ret_.add(str);
+						ret_.add("+");
+						str = "-";
+					} else {
+						str = "-";
+					}
+				} else {
+					if (!str.equals(""))
+						ret_.add(str);
+					str = "";
+					ret_.add(xar.toString());
+				}
 			} else {
 				str += xar;
 			}
@@ -57,6 +96,19 @@ public class EquationParser {
 				ret.add(str2);
 			}
 		}
+		// Separating "-"s from stuff
+		boolean changed = true;
+		while (changed) {
+			changed = false;
+			for (int i = 0; i < ret.size(); i++) {
+				if (ret.get(i).length() > 1 && ret.get(i).startsWith("-") && !isNumber(Character.toString(ret.get(i).charAt(1)))) {
+					ret.set(i, ret.get(i).substring(1));
+					ret.add(i, "-");
+					ret.add(i, "+");
+					changed = true;
+				}
+			}
+		}
 		// Replacing {} & [] into ()
 		String[] array1 = { "[", "{" };
 		List<String> list1 = new ArrayList<String>();
@@ -82,11 +134,72 @@ public class EquationParser {
 				ret.add(i, Double.toString(Math.PI));
 			}
 		}
+		// Remove emptys
+		for (int i = 0; i < ret.size(); i++) {
+			if (ret.get(i).equals("")) {
+				ret.remove(i);
+				i--;
+			}
+		}
+		// Remove "+"s next to parentheses
+		for (int i = 0; i < ret.size(); i++) {
+			if (i != 0 && ret.get(i).equals(")") && ret.get(i - 1).equals("+")) {
+				ret.remove(i - 1);
+				i--;
+			} else if (i != ret.size() - 1 && ret.get(i).equals("(") && ret.get(i + 1).equals("+")) {
+				ret.remove(i + 1);
+			}
+		}
+		// Do signal games
+		changed = true;
+		while (changed) {
+			changed = false;
+			// ++
+			for (int i = 0; i < ret.size(); i++) {
+				if (i != ret.size() - 1 && ret.get(i).equals("+") && ret.get(i + 1).equals("+")) {
+					ret.remove(i);
+					changed = true;
+				}
+			}
+			// --
+			for (int i = 0; i < ret.size(); i++) {
+				if (i != ret.size() - 1 && ret.get(i).equals("-") && ret.get(i + 1).equals("-")) {
+					ret.remove(i);
+					ret.set(i, "+");
+					changed = true;
+				}
+			}
+			// +-
+			for (int i = 0; i < ret.size(); i++) {
+				if (i != ret.size() - 1 && ((ret.get(i).equals("+") && ret.get(i + 1).equals("-")) || (ret.get(i).equals("-") && ret.get(i + 1).equals("+")))) {
+					ret.remove(i);
+					ret.set(i, "-");
+					changed = true;
+				}
+			}
+		}
+		// Removing some extra junk
+		// At beginning
+		while (true) {
+			if ((ret.get(0).equals("-") && isNumber(ret.get(1))) || ret.get(0).equals("+")) {
+				ret.remove(0);
+			} else {
+				break;
+			}
+		}
+			// At end
+		while (true) {
+			if (ret.get(ret.size() - 1).equals("-") || ret.get(ret.size() - 1).equals("+")) {
+				ret.remove(ret.size() - 1);
+			} else {
+				break;
+			}
+		}
 		return ret;
 	}
 
 	public static List<String> ops() {
-		String[] array = { "=", "+", "-", "*", "/", "(", ")", "[", "]", "{", "}", "^" , "log", "sin", "cos", "tg", "!" };
+		String[] array = { "=", "+", "-", "*", "/", "(", ")", "[", "]", "{", "}", "^", "ln", "log", "sin", "cos", "tg", "!" };
 		List<String> ret = new ArrayList<String>();
 		for (String i : array) {
 			ret.add(i);
